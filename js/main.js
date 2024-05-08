dbb15e19eec1515b9aa036a2840cc72559f41f3d
nathan.allemann <nathan.allemann@divcom.ch>
2024-05-08 16:05:52 +0200

    Ajout d'une ligne de tableau HTML depuis le javascript

--- js/main.js
+++ js/main.js
@@ -37,3 +37,44 @@ const personnes = [
         localite: 'San Francisco',
     }
 ];
+
+//Récuperer le premier formulaire du document
+const formulaire = document.querySelector('form');
+
+//Recupere le champ texte #nom et #prenom et #age et #localite
+const txtNom = document.querySelector('#nom');
+const txtPrenom = document.querySelector('#prenom');
+const txtAge = document.querySelector('#age');
+const txtLocalite = document.querySelector('#localite');
+
+//Recupere le corps du tableau <tbody>
+const tablebody = document.querySelector('tbody')
+
+//tester de la recuperation des données HTML est ok
+console.log(formulaire, txtNom, tablebody);
+
+console.log(formulaire);
+
+//Ecouter l'envoi du formulaire
+
+formulaire.addEventListener('submit', function envoyer(event) {
+    //Stopper l'envoi du formulaire
+    event.preventDefault();
+
+    //Créer le code HTML d'une nouvelle ligne de tableau
+    let nouvelleLigne =
+        `<tr>
+            <td>${ txtPrenom.value }</td>
+            <td>${ txtNom.value }</td>
+            <td>${ txtAge.value }</td>
+            <td>${ txtLocalite.value }</td>
+        </tr>`;
+
+    //ajouter la nouvelle personne à la fin du <tbody>
+    tablebody.innerHTML += nouvelleLigne;
+
+    //vide le formulaire
+    formulaire.reset();
+
+    //
+});
