b1afccc8e23c2223bf9f863e19d180ef57da65c0
nathan.allemann <nathan.allemann@divcom.ch>
2024-05-22 15:25:10 +0200

    Nouvel ajout d'un tableau

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@
 
 'use strict';
 
-const personnes = [
+const tabPersonnes = [
     {
         nom: 'Doe',
         prenom: 'John',
@@ -38,43 +38,54 @@ const personnes = [
     }
 ];
 
-//Récuperer le premier formulaire du document
+// Récupère le 1er formulaire du document
 const formulaire = document.querySelector('form');
-
-//Recupere le champ texte #nom et #prenom et #age et #localite
+// Récupère les champs textes
 const txtNom = document.querySelector('#nom');
 const txtPrenom = document.querySelector('#prenom');
 const txtAge = document.querySelector('#age');
 const txtLocalite = document.querySelector('#localite');
+// Récupère le premier corps de tableau <tbody>
+const tableBody = document.querySelector('tbody');
+// Test si la récupération des éléments HTML est OK
+console.log(formulaire, txtNom, tableBody);
 
-//Recupere le corps du tableau <tbody>
-const tablebody = document.querySelector('tbody')
+//fonction qui créé un tableau HTML a partir du tableau JS des personnes
+function construireTableau()
+{
+    //Vide le tableau
+    tableBody.innerHTML = "";
 
-//tester de la recuperation des données HTML est ok
-console.log(formulaire, txtNom, tablebody);
+    //Parcours le tableau d'objets tabPersonnes
+    for( let personne of tabPersonnes) {
+        tableBody.innerHTML+= `
+        <tr>
+            <td>${ personne.prenom }</td>
+            <td>${ personne.nom }</td>
+            <td>${ personne.age }</td>
+            <td>${ personne.localite }</td>
+        </tr>`;
+    }
+}
 
-console.log(formulaire);
+//Ecoute la fin de chargement de page
+window.addEventListener('load', construireTableau)
 
-//Ecouter l'envoi du formulaire
-
+// Ecouter l'envoi du formulaire
 formulaire.addEventListener('submit', function envoyer(event) {
-    //Stopper l'envoi du formulaire
+    // Stopper l'envoi du formulaire
     event.preventDefault();
 
-    //Créer le code HTML d'une nouvelle ligne de tableau
-    let nouvelleLigne =
-        `<tr>
-            <td>${ txtPrenom.value }</td>
-            <td>${ txtNom.value }</td>
-            <td>${ txtAge.value }</td>
-            <td>${ txtLocalite.value }</td>
-        </tr>`;
-
-    //ajouter la nouvelle personne à la fin du <tbody>
-    tablebody.innerHTML += nouvelleLigne;
-
-    //vide le formulaire
+    //Ajoute un objet à tabPersonnes
+    tabPersonnes.push({
+        prenom: txtPrenom.value,
+        nom: txtNom.value,
+        age: txtAge.value,
+        localite: txtLocalite.value,
+    });
+    construireTableau();
+    // Vide tous les champs du formulaire
     formulaire.reset();
-
-    //
+    // Focus sur le prénom
+    txtPrenom.focus();
 });
